11f5a277c86ec017e61e5cb08620f848cf5bbc16
Mikka <i@cvyl.me>
2025-07-13 02:44:50 +0200

    style: prettier

--- js/theme.js
+++ js/theme.js
@@ -3,19 +3,19 @@ const lightInput = document.getElementById("light");
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme === "light") {
-    lightInput.checked = true;
+  lightInput.checked = true;
 } else {
-    darkInput.checked = true;
+  darkInput.checked = true;
 }
 
 darkInput.addEventListener("change", () => {
-    if (darkInput.checked) {
-        localStorage.setItem("theme", "dark");
-    }
+  if (darkInput.checked) {
+    localStorage.setItem("theme", "dark");
+  }
 });
 
 lightInput.addEventListener("change", () => {
-    if (lightInput.checked) {
-        localStorage.setItem("theme", "light");
-    }
+  if (lightInput.checked) {
+    localStorage.setItem("theme", "light");
+  }
 });
