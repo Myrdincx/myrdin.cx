332ed51506ae3b15def34d9fd769049d9b1eb068
Myrdincx <myrdincx@gmail.com>
2025-07-15 00:40:55 +0200

    navigation!!!

--- js/modalview.js
+++ js/modalview.js
@@ -2,16 +2,42 @@ const modal = document.getElementById("imageModal");
 const modalImg = document.getElementById("modalImg");
 const openLink = document.getElementById("openInNewTab");
 
-document.querySelectorAll(".gallery img").forEach((img) => {
+const images = Array.from(document.querySelectorAll(".gallery img"));
+let currentIndex = -1;
+
+images.forEach((img, index) => {
   img.addEventListener("click", () => {
-    modalImg.src = img.src;
-    openLink.href = img.src;
-    modal.style.display = "flex";
+    currentIndex = index;
+    openModalWithImage(img.src);
   });
 });
+
+function openModalWithImage(src) {
+  modalImg.src = src;
+  openLink.href = src;
+  modal.style.display = "flex";
+}
 
 function closeModal(event) {
   if (!event.target.closest("img") && !event.target.closest("a")) {
     modal.style.display = "none";
+  }
+}
+
+function showPrevImage(event) {
+  event.stopPropagation();
+  if (currentIndex > 0) {
+    currentIndex--;
+    const newSrc = images[currentIndex].src;
+    openModalWithImage(newSrc);
+  }
+}
+
+function showNextImage(event) {
+  event.stopPropagation();
+  if (currentIndex < images.length - 1) {
+    currentIndex++;
+    const newSrc = images[currentIndex].src;
+    openModalWithImage(newSrc);
   }
 }
